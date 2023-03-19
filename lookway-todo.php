a0db6b182da9352d506a9fe426d5026a5ec29c5f
Sergio Hardov <sergiohardov@gmail.com>
2023-03-19 17:23:11 +0200

    Создана функция добавления пункта меню в админке + страница плагина

--- lookway-todo.php
+++ lookway-todo.php
@@ -17,6 +17,12 @@ define('LOOKWAY_TODO_PATH', plugin_dir_path(__FILE__));
 
 class Lookway_Todo
 {
+    public function __construct()
+    {
+        // Add link on plugin page in admin sidebar
+        add_action('admin_menu', [$this, 'plugin_link_menu']);
+    }
+
     static function activation()
     {
         flush_rewrite_rules();
@@ -25,6 +31,24 @@ class Lookway_Todo
     static function deactivation()
     {
         flush_rewrite_rules();
+    }
+
+    public function plugin_main_page()
+    {
+        require_once LOOKWAY_TODO_PATH . 'admin/index.php';
+    }
+
+    public function plugin_link_menu()
+    {
+        add_menu_page(
+            esc_html__('Lookway ToDo Page', 'lookway-todo'),
+            esc_html__('Lookway ToDo', 'lookway-todo'),
+            'manage_options',
+            'lookway_todo_plugin_main_page',
+            [$this, 'plugin_main_page'],
+            'dashicons-admin-plugins',
+            100
+        );
     }
 }
 
