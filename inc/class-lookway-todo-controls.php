b1f296b1de2257d70d68e76c0ea647fd7593300d
Sergio Hardov <sergiohardov@gmail.com>
2023-03-19 21:03:36 +0200

    Создан класс с методами создания и удаления тасков

---
+++ inc/class-lookway-todo-controls.php
@@ -0,0 +1,36 @@
+<?php
+class Lookway_Todo_Controls
+{
+    static function create_new_task($title, $desc)
+    {
+        $args = [
+            'post_title' => $title,
+            'post_content' => $desc,
+            'post_status' => 'publish',
+            'post_type' => 'task'
+        ];
+
+        $post_id = wp_insert_post($args);
+
+        if ($post_id > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static function delete_all_task()
+    {
+        $tasks = get_posts([
+            'post_type' => 'task', 'numberposts' => -1
+        ]);
+
+        foreach ($tasks as $task) {
+            wp_delete_post($task->ID, true);
+        }
+
+        return true;
+    }
+}
+
+// $Lookway_Todo_Controls = new Lookway_Todo_Controls();
